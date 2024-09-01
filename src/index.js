d86cc7c14332ea8bb8c76fcb7dd99e1bd4044e50
shivam24161 <shivam24161@gmail.com>
2024-09-01 15:41:12 +0530

    skelton added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Context from "./context/Context";
+import 'react-loading-skeleton/dist/skeleton.css'
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
